2252e9dac65424ca69665bf031de8bfb3e522dcc
Larko_0 <larionov1001@gmail.com>
2021-02-18 15:02:53 +0200

    Prop added

--- django_app/myfirstapp/models.py
+++ django_app/myfirstapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import date, timedelta
+
 
 # Create your models here.
 
@@ -8,106 +10,106 @@ class UOM(models.Model):
     '''
     # название еденицы измерения
     name = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name = "Еденицы измерения"
         verbose_name_plural = verbose_name
-    
-    
-    
-    
+
+
 class Nomenclature(models.Model):
     '''
     Номенклатура - включает материалы и готовую продукци
     '''
     name = models.CharField(max_length=50)
     uom = models.ForeignKey(UOM, on_delete=models.PROTECT)
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name = "Номенклатура материалов и ГП"
         verbose_name_plural = verbose_name
-    
-    
-class Counterparty (models.Model):
+
+
+class Counterparty(models.Model):
     '''
     список контрагентов
     '''
     name = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.name
 
-    class Meta: 
+    class Meta:
         # Add verbose name 
         verbose_name = 'Контрагенты'
         verbose_name_plural = verbose_name
-        
+
 
 class ConversionRate(models.Model):
     '''    
     коэффециенты конверсии 1 base_uom = conversion_rate of target_uom
     '''
     material = models.ForeignKey(Nomenclature, on_delete=models.PROTECT)
-    base_uom = models.ForeignKey(UOM, 
-                    on_delete=models.PROTECT, 
-                    related_name='covn_base_uom')
-    target_uom = models.ForeignKey(UOM, on_delete=models.PROTECT, 
+    base_uom = models.ForeignKey(UOM,
+                                 on_delete=models.PROTECT,
+                                 related_name='covn_base_uom')
+    target_uom = models.ForeignKey(UOM, on_delete=models.PROTECT,
                                    related_name='covn_target_uom')
-    conversion_rate = models.DecimalField(max_digits = 10, 
-                                          decimal_places = 4)
-    
+    conversion_rate = models.DecimalField(max_digits=10,
+                                          decimal_places=4)
+
     def __str__(self):
-        return f"{self.material} {self.base_uom}"\
-                f" {self.target_uom} {self.conversion_rate}"
-    
+        return f"{self.material} {self.base_uom}" \
+               f" {self.target_uom} {self.conversion_rate}"
+
     class Meta:
         verbose_name = "Коэффециент конверсии"
         verbose_name_plural = verbose_name
 
+
 class Purchase(models.Model):
-    '''
+    """
     Таблица закупок
-    '''
+    """
     purch_date = models.DateField()
     counterparty = models.ForeignKey(Counterparty, on_delete=models.PROTECT)
     material = models.ForeignKey(Nomenclature, on_delete=models.PROTECT)
     purch_uom = models.ForeignKey(UOM, on_delete=models.PROTECT)
     quantity = models.DecimalField(max_digits=10, decimal_places=3)
-    price_ex_vat = models.DecimalField(max_digits=10, decimal_places = 3)
-    price_with_vat = models.DecimalField(max_digits=10, decimal_places = 3)
+    price_ex_vat = models.DecimalField(max_digits=10, decimal_places=3)
+    price_with_vat = models.DecimalField(max_digits=10, decimal_places=3)
 
     def __str__(self):
         return f"{self.counterparty} - {self.material} - {self.purch_date}"
-    
+
     class Meta:
         verbose_name = "Закупки"
         verbose_name_plural = verbose_name
-        
-        
+
+
 class Production(models.Model):
-    '''
+    """
     отчет производства
-    '''
+    """
     prod_date = models.DateField()
     description = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return f"{self.description} - {self.prod_date}"
-    
+
     class Meta:
         verbose_name = "отчет производства"
         verbose_name_plural = verbose_name
-    
+
+
 class ProductionProduced(models.Model):
-    '''
+    """
     Номенклатура, которая была получена в результате производства
-    '''
+    """
     prod_report = models.ForeignKey(Production, on_delete=models.CASCADE)
     produced = models.ForeignKey(Nomenclature, on_delete=models.PROTECT)
     produced_uom = models.ForeignKey(UOM, on_delete=models.PROTECT)
@@ -115,26 +117,34 @@ class ProductionProduced(models.Model):
 
     def __str__(self):
         return f"{self.prod_report} - {self.produced} - {self.quantity}"
-    
+
     class Meta:
         verbose_name = "Произведенная продукция"
         verbose_name_plural = verbose_name
-        
-        
+
+
 class ProductionConsumed(models.Model):
-    '''
+    """
     Номенклатура, которая была списана в результате производства
     и связанная с ProductionProduced
-    '''
-    
+    """
+
     produced = models.ForeignKey(ProductionProduced, on_delete=models.CASCADE)
     consumed = models.ForeignKey(Nomenclature, on_delete=models.PROTECT)
     consumed_uom = models.ForeignKey(UOM, on_delete=models.PROTECT)
     quantity = models.DecimalField(max_digits=10, decimal_places=3)
-    
+
     def __str__(self):
-        return f"{self.produced} - {self.consumed} - {self.quantity}"
-    
+        return f"{self.produced} - {self.consumed} - {self.quantity} !{self.average_purchase_price}!"
+
     class Meta:
         verbose_name = "Списанные материалы"
         verbose_name_plural = verbose_name
+
+    @property
+    def average_purchase_price(self):
+        date_X = self.produced.prod_report.prod_date
+        start_date = date_X - timedelta(days=30)
+        purchases = Purchase.objects.filter(material_id=self.consumed.id)
+        last_purchases = list(filter(lambda purchase: purchase.purch_date >= start_date, purchases))
+        return sum(map(lambda purchase: purchase.price_ex_vat, last_purchases)) / sum(map(lambda purchase: purchase.quantity, last_purchases))
